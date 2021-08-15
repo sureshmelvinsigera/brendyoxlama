7eb79843c5f7a5666c177c32db83fd92e3057c6b
brendsupport <79204947+brendsupport@users.noreply.github.com>
2021-08-15 13:55:59 +0000

    Update __main__.py

--- brend_installer/__main__.py
+++ brend_installer/__main__.py
@@ -100,7 +100,6 @@ if __name__ == "__main__":
     #Əkənin varyoxunu sikim peyser ble
     #Peyserler:
     #1) Fərid - https://github.com/FaridDadashzade/Installer/blob/1ed448a46d35f4bea4af87ee3b6ba6d684a3e990/cyber_installer/__main__.py#L107
-    #2) F16 
     if os.path.isdir("./brenduserbot/"):
         rm_r("./brenduserbot/")
     repo = eval('Repo.clone_from("https://github.com/brendsupport/brenduserbot", "./brenduserbot/", branch="master")')
